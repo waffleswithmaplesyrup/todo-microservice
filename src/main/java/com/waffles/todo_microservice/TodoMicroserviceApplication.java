44bd5c836b334f73f7be7c0ba9510938a6027a86
Zahra <zahra.mohamed.n7@gmail.com>
2025-06-29 12:16:02 +0800

    First commit

---
+++ src/main/java/com/waffles/todo_microservice/TodoMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.waffles.todo_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waffles/todo_microservice/TodoMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waffles.todo_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
